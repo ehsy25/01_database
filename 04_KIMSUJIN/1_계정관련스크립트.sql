1da740860e8b77160188cd03055c054573bb7fe9
whatimeisitnow <jayjay.hi.77@gmail.com>
2025-09-24 17:50:33 +0900

    0924 sujin

---
+++ 04_KIMSUJIN/1_계정관련스크립트.sql
@@ -0,0 +1,11 @@
+select * from user;
+create database menudb;
+SHOW GRANTS FOR 'swcamp'@'%';
+GRANT ALL PRIVILEGES ON menudb.* TO 'swcamp'@'%';
+
+
+
+SHOW GRANTS FOR 'swcamp'@'%';
+create database employeedb;
+show databases;
+GRANT ALL PRIVILEGES ON employeedb.* TO 'swcamp'@'%';
